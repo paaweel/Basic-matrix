5659bf39b366db78f9a2aaef2d02ffba7e56e26f
Pawel <pol4_1997@o2.pl>
2017-09-12 20:58:31 +0200

    Added Vector header

---
+++ Vector.h
@@ -0,0 +1,45 @@
+#ifndef VECTOR
+#define VECTOR
+
+#include <iostream>
+
+class Vector {
+  int *vec;
+  int size;
+public:
+  //Costructors
+  Vector();                                     //default
+  Vector(const Vector & v);                     //copy
+  Vector(const int n);                          //n-sized, with pre-declared value
+  Vector(const int n, const int value);         //n-sized, value passed as argument
+
+  //Destructor
+  ~Vector();
+  //Getters
+  int getSize();
+
+  //math
+  Vector operator+ (const Vector & v1);         //natural addition
+  Vector operator- (const Vector & v1);         //natural subtraction
+  Vector operator* (const int n);               //multiply by a number
+  Vector operator* (const Vector & v1);         //multiply elements respectivly
+  Vector & operator+= (const Vector & v1);      //same as +, but in-place
+  Vector & operator-= (const Vector & v1);      //same as -, but in-place
+  Vector & operator*= (const int n);            //same as * integer, but in-place
+  Vector & operator*= (const Vector & v1);      //same as *, but in-place
+
+  //logic
+  bool operator== (const Vector &v1);
+  bool operator!= (const Vector &v1);
+
+  //in/out
+  friend std::ostream operator<< (std::ostream out, const Vector &v1);
+  friend std::istream operator>> (std::istream in, const Vector &v1);
+
+  //index
+  int operator[] (const int i);                 //access i-th element
+
+};
+
+
+#endif
