b221dde397e14c4686182cd9738104b7f96ebbb5
Pawel <pol4_1997@o2.pl>
2017-09-15 21:32:56 +0200

    Added remaining operators and destructor for Matrix class

--- Matrix.cpp
+++ Matrix.cpp
@@ -28,6 +28,15 @@ Matrix::Matrix (const int newRows, const int newColumns, const int value) : rows
   }
 }
 
+//destructor
+Matrix::~Matrix() {
+  for (int i = 0; i < rows; ++i)
+    delete matrix[i];
+  delete matrix;
+}
+
+
+//in/out
 std::ostream & operator<< (std::ostream &out, const Matrix & m1) {
   for (int i = 0; i < m1.rows; ++i)
     out << *m1.matrix[i];
@@ -40,10 +49,123 @@ std::istream & operator>> (std::istream &in, Matrix & m1) {
   return in;
 }
 
+//getters
 int Matrix::getRows () {
   return rows;
 }
 
 int Matrix::getColumns () {
   return columns;
+}
+
+//math
+Matrix Matrix::operator+ (const Matrix & m1) {
+  if (m1.rows != rows || m1.columns != columns) {
+    std::cout << "Sizes do not match\n"
+              << "Exit\n";
+    exit(1);
+  }
+
+  Matrix sum (m1);
+  for (int i = 0;  i < rows; ++i)
+    *sum.matrix[i] += *matrix[i];
+  return sum;
+}
+
+Matrix Matrix::operator- (const Matrix & m1) {
+  if (m1.rows != rows || m1.columns != columns) {
+    std::cout << "Sizes do not match\n"
+              << "Exit\n";
+    exit(1);
+  }
+
+  Matrix difference (m1);
+  for (int i = 0;  i < rows; ++i)
+    *difference.matrix[i] -= *matrix[i];
+  return difference;
+}
+
+Matrix Matrix::operator* (const int n) {
+  for (int i = 0;  i < rows; ++i)
+    *matrix[i] *= n;
+  return *this;
+}
+
+Matrix & Matrix::operator+= (const Matrix & m1) {
+  if (m1.rows != rows || m1.columns != columns) {
+    std::cout << "Sizes do not match\n"
+              << "Exit\n";
+    exit(1);
+  }
+
+  for (int i = 0;  i < rows; ++i)
+    *matrix[i] += *m1.matrix[i];
+  return *this;
+}
+
+Matrix & Matrix::operator-= (const Matrix & m1) {
+  if (m1.rows != rows || m1.columns != columns) {
+    std::cout << "Sizes do not match\n"
+              << "Exit\n";
+    exit(1);
+  }
+
+  for (int i = 0;  i < rows; ++i)
+    *matrix[i] -= *m1.matrix[i];
+  return *this;
+}
+
+Matrix & Matrix::operator*= (const int n) {
+  for (int i = 0;  i < rows; ++i)
+    *matrix[i] *= n;
+  return *this;
+}
+
+//logic
+bool Matrix::operator== (const Matrix & m1) {
+  if (m1.rows != rows || m1.columns != columns) {
+    return false;
+  }
+  for (int i = 0; i < rows; ++i) {
+    if (*matrix[i] != *m1.matrix[i])
+      return false;
+  }
+  return true;
+}
+
+bool Matrix::operator!= (const Matrix & m1) {
+  if (m1.rows != rows || m1.columns != columns) {
+    return true;
+  }
+  for (int i = 0; i < rows; ++i) {
+    if (*matrix[i] != *m1.matrix[i])
+      return true;
+  }
+  return false;
+}
+
+//index
+Vector & Matrix::operator[] (const int i) {
+  return *matrix[i];
+}
+
+Matrix & Matrix::operator= (const Matrix & m1) {
+  if (rows == m1.rows) {
+    for (int i = 0; i < rows; ++i) {
+      *matrix[i] = *m1.matrix[i];
+    }
+    return *this;
+  }
+
+  for (int i = 0; i < rows; ++i)
+    delete matrix[i];
+  delete matrix;
+
+  rows = m1.rows;
+  columns = m1.columns;
+  matrix = new Vector* [rows];
+  for (int i = 0; i < rows; ++i) {
+    matrix[i] = new Vector (*m1.matrix[i]);
+  }
+  return *this;
 }
--- Matrix.h
+++ Matrix.h
@@ -15,11 +15,33 @@ public:
   Matrix (const int newRows, const int newColumns);                   //arguments defined size
   Matrix (const int newRows, const int newColumns, const int value);  //same as above, but with a value
 
+  //destructor
+  ~Matrix();
+  //geters
   int getRows ();
   int getColumns();
 
+  //in/out
   friend std::ostream & operator<< (std::ostream &out, const Matrix & m1);
   friend std::istream & operator>> (std::istream &in, Matrix & m1);
+
+  //math
+  Matrix operator+ (const Matrix & m1);
+  Matrix operator- (const Matrix & m1);
+  Matrix operator* (const int n);
+  Matrix & operator+= (const Matrix & m1);
+  Matrix & operator-= (const Matrix & m1);
+  Matrix & operator*= (const int n);
+
+  //logic
+  bool operator== (const Matrix & m1);
+  bool operator!= (const Matrix & m1);
+
+  //index
+  Vector & operator[] (const int i);
+
+  //assignment
+  Matrix & operator= (const Matrix & m1);
 
 };
 
