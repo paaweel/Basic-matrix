2d686878b13224c0a89e14dd0007a6c84bf49841
Pawel <pol4_1997@o2.pl>
2017-09-15 14:29:46 +0200

    Matrix class => header, constructors and overloaded << and >>

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,1 +1,41 @@
 #include "Matrix.h"
+
+Matrix::Matrix () : rows {2}, columns {2} {
+  matrix = new Vector* [rows];
+  for (int i = 0; i < rows; ++i) {
+    matrix[i] = new Vector (columns);
+  }
+}
+
+Matrix::Matrix (const Matrix &m1) : rows {m1.rows}, columns {m1.columns} {
+  matrix = new Vector* [rows];
+  for (int i = 0; i < rows; ++i) {
+    matrix[i] = new Vector (*m1.matrix[i]);
+  }
+}
+
+Matrix::Matrix (const int newRows, const int newColumns) : rows {newRows}, columns {newColumns} {
+  matrix = new Vector* [rows];
+  for (int i = 0; i < rows; ++i) {
+    matrix[i] = new Vector (columns);
+  }
+}
+
+Matrix::Matrix (const int newRows, const int newColumns, const int value) : rows {newRows}, columns {newColumns} {
+  matrix = new Vector* [rows];
+  for (int i = 0; i < rows; ++i) {
+    matrix[i] = new Vector (columns, value);
+  }
+}
+
+std::ostream & operator<< (std::ostream &out, const Matrix & m1) {
+  for (int i = 0; i < m1.rows; ++i)
+    out << *m1.matrix[i];
+  return out;
+}
+
+std::istream & operator>> (std::istream &in, Matrix & m1) {
+  for (int i = 0; i < m1.rows; ++i)
+    in >> *m1.matrix[i];
+  return in;
+}
--- Matrix.h
+++ Matrix.h
@@ -3,6 +3,24 @@
 
 #include "Vector.h"
 
+class Matrix {
+  Vector **matrix;
+  int rows;
+  int columns;
 
+public:
+  //constructors
+  Matrix ();                                                          //default
+  Matrix (const Matrix &m1);                                          //copy
+  Matrix (const int newRows, const int newColumns);                   //arguments defined size
+  Matrix (const int newRows, const int newColumns, const int value);  //same as above, but with a value
+
+  int getRows ();
+  int getColumns();
+
+  friend std::ostream & operator<< (std::ostream &out, const Matrix & m1);
+  friend std::istream & operator>> (std::istream &in, Matrix & m1);
+
+};
 
 #endif
